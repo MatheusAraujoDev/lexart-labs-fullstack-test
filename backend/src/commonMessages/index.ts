87d168c0d3074ea261c49047346a0328a5fe839e
MatheusAraujoDev <matheusaraujo.dev@gmail.com>
2023-08-29 17:10:51 -0300

    Fix: Menu options messages

--- backend/src/commonMessages/index.ts
+++ backend/src/commonMessages/index.ts
@@ -26,9 +26,9 @@ export const errorAnswer = (data: IChatHistory) => {
 };
 
 const loansMessages: Record<string,string> = {
-	'do you want to apply for a loan?': '<a href="https://lexartlabs.com/" target="_blank">Answer for option 1</a>',
-	'loan conditions': '<a href="https://lexartlabs.com/" target="_blank">Answer for option 2</a>',
-	'help': '<a href="https://lexartlabs.com/" target="_blank">Answer for option 3</a>'
+	'do you want to apply for a loan?': 'Here is the best personal loans for August 2023. <a href="https://www.forbes.com/advisor/personal-loans/best-personal-loans/" target="_blank">Reference</a>',
+	'loan conditions': 'You can learn more about loan conditions on this <a href="https://www.forbes.com/advisor/personal-loans/personal-loan-requirements/" target="_blank">reference</a>.',
+	'help': '<a href="https://lexartlabs.com/" target="_blank">learn more</a>'
 };
 
 export const loans = (data: IChatHistory) => {
